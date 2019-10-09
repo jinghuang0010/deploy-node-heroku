ca80afc1de81136bb742a8366b80bf226df89538
jinghuang0010 <jing.huang0010@stud.hkr.se>
2019-10-09 17:53:26 +0200

    vv4

--- server.js
+++ server.js
@@ -1,42 +1,6 @@
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
+const express = require('express')
+const app = express()
 
-//设置连接
-var connection =mysql.createConnection({
+app.use(express.static('public'))
 
-	host: 'localhost',
-	user: 'root',
-	password: '1234',
-	database: 'huangjing'
-});
-
-connection.connect(function(error){
-	if(!!error){
-		console.log('Error');
-	}else{
-		console.log('Connected');
-	}
-
-});
-
-
-
-app.get('/', function(req,resp){
-	 connection.query("select * from measurement where unit_id>(select count(*) from measurement)-5 order by unit_id asc limit 5",function(error,rows,fields){
-	    if(!!error){
-		console.log('Error in the query');
-	}else{
-		console.log('Succeseryl query\n');
-		return resp.json({
-			data:rows
-		})
-
-	}
-});
-})
-
-
-
-
-app.listen(8080);
+app.listen(process.env.PORT || 8080, () => console.log("All is ok!"))
