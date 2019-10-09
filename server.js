253d5ea863fcb00c477a1d91a47a8c908a70b3c6
jinghuang0010 <jing.huang0010@stud.hkr.se>
2019-10-09 22:44:51 +0200

    v66

--- server.js
+++ server.js
@@ -1,35 +1,6 @@
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
-var connection =mysql.createConnection({
-	
-	host: 'us-cdbr-iron-east-05.cleardb.net',
-	user: 'b23a34da9fe379',
-	password: '68882ca0',
-	database: 'heroku_c595c5d7e6b1b97'
-});
+const express = require('express')
+const app = express()
 
-connection.connect(function(error){
-	if(!!error){
-		console.log('Error');
-	}else{
-		console.log('Connected');
-	}
-	
-});
+app.use(express.static('public'))
 
-
-
-app.get('/', function(req,resp){
-	 connection.query("select * from jing",function(error,rows,fields){
-	    if(!!error){
-		console.log('Error in the query');
-	}else{
-		console.log('Succeseryl query\n');
-		console.log(rows);
-		resp.send('Hello, ' + JSON.stringify(rows));
-		
-	}
-});
-})
-app.listen(1337);
+app.listen(process.env.PORT || 8080, () => console.log("All is ok!"))
