bd7715ddfcd78e9795a980e8d28c8a719b44e42c
jinghuang0010 <jing.huang0010@stud.hkr.se>
2019-10-09 12:15:38 +0200

    vv3

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
 
-app.use(express.static('public'))
+//设置连接
+var connection =mysql.createConnection({
 
-app.listen(process.env.PORT || 8080, () => console.log("All is ok!"))
+	host: 'localhost',
+	user: 'root',
+	password: '1234',
+	database: 'huangjing'
+});
+
+connection.connect(function(error){
+	if(!!error){
+		console.log('Error');
+	}else{
+		console.log('Connected');
+	}
+
+});
+
+
+
+app.get('/', function(req,resp){
+	 connection.query("select * from measurement where unit_id>(select count(*) from measurement)-5 order by unit_id asc limit 5",function(error,rows,fields){
+	    if(!!error){
+		console.log('Error in the query');
+	}else{
+		console.log('Succeseryl query\n');
+		return resp.json({
+			data:rows
+		})
+
+	}
+});
+})
+
+
+
+
+app.listen(8080);
