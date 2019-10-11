2f7fd663a9a924b359387cb7590bbc3d99e0d2a0
jinghuang0010 <jing.huang0010@stud.hkr.se>
2019-10-11 12:47:21 +0200

    ll

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
 
 
-var connection =mysql.createConnection({
-	
-	host: 'us-cdbr-iron-east-05.cleardb.net',
-	user: 'b23a34da9fe379',
-	password: '68882ca0',
-	database: 'heroku_c595c5d7e6b1b97'
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b23a34da9fe379',
+    password: '68882ca0',
+    database: 'heroku_c595c5d7e6b1b97'
+  });
+  app.get('/', function(request,response){
+  pool.query('select * from jing', function (err, rows, fields) {
+    if(err){
+        console.log('error',err);
+        throw err;
+    }
+        response.send(['hello jing!!!',rows]);
+  });
 });
 
-connection.connect();
-
-
-
-
-
-app.get('/', function(request,response){
-	 connection.query('select * from jing',function(err,rows,fields){
-	    if(err){
-		console.log('error',err);
-		throw err;
-	}
-		response.send(['hello world!!!',rows]);
-		
-	
-});
-});
-var port =process.env.PORT || 5000;
+var port =process.env.PORT || 5000;
 
 app.listen(port,function(){
-	console.log("listen on" + port);
+    console.log("listen on" + port);
 });
